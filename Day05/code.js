3dd9f2a0f7fc7e8d65d9858bbcd1fbf7f493c233
VaibhaV Arde <vaibhav.arde@gmail.com>
2026-06-18 20:44:19 +0530

    Add Git commands for repository setup
    Add initial Git commands and repository setup instructions.

---
+++ Day05/code.js
@@ -0,0 +1,13 @@
+// Git Repo-> Clone -> Local 
+
+// Local Repo -> add git rpo url -> GitHub
+
+// git init
+// git add .
+// git commit -m "message"
+// git remote add origin <url>
+// git push -u origin main
+
+// git remote add origin https://github.com/vaibhav-arde/Playwright9thJuneBatch.git
+// git branch -M main
+// git push -u origin main
